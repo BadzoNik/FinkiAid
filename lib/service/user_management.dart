333ebf12c8df032284808bf08578b8541750be33
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2024-02-12 19:22:48 +0100

    bug fixed when user logging via google account

--- lib/service/user_management.dart
+++ lib/service/user_management.dart
@@ -4,18 +4,26 @@ import 'package:flutter/cupertino.dart';
 import '../model/UserFinki.dart';
 
 class UserManagement {
-  void storeNewUser(UserFinki user, BuildContext context) {
-    FirebaseFirestore.instance.collection('users')
-        .add({
+  Future<void> storeNewUser(UserFinki user, BuildContext context) async {
+    final QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: user.email)
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      Navigator.of(context).pop();
+      Navigator.of(context).pushReplacementNamed('/home');
+      return;
+    }
+
+    await FirebaseFirestore.instance.collection('users').add({
       'name': user.name,
       'surname': user.surname,
       'email': user.email,
       'role': user.userRole.name
-    }).then((value) {
-      Navigator.of(context).pop();
-      Navigator.of(context).pushReplacementNamed('/home');
-    }).catchError((e) {
-      debugPrint("Error in user management: $e");
     });
+
+    Navigator.of(context).pop();
+    Navigator.of(context).pushReplacementNamed('/home');
   }
 }
